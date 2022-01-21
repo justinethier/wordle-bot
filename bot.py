88879b6cd44c64920bd289bfb7e48a2d4afe7e1d
Justin Ethier <justin.ethier@gmail.com>
2022-01-20 20:02:57 -0800

    Added scaffold

---
+++ bot.py
@@ -0,0 +1,11 @@
+# TODO: using sqlite3 DB -
+# select * from word_counts where first = 'a' and word like '%e%' and word like '%b';
+#
+# for correct letters (0) - use first, second, etc
+# for wrong position (1) - use 'like' clause
+#
+# TODO: how to pick a word from the list. brute force with first one, for now???
+
+import check
+
+
