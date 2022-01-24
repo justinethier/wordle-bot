a3e79d9eabce321d3194d2ed8854f0980b63f10e
Justin Ethier <justin.ethier@gmail.com>
2022-01-24 09:10:54 -0800

    Generate DB

--- create-db.sh
+++ create-db.sh
@@ -9,12 +9,12 @@ function csv_to_sqlite() {
 #database_file_name=$1
 #table_name=$2
 
-database_file_name=test.db
+database_file_name=words.db
 table_name=words
 
 rm -f $database_file_name
 sqlite3 $database_file_name "create table words (\"word\" text);"
-sqlite3 $database_file_name "create table word_counts (\"word\" text, \"first\" text, \"second\" text, \"third \" text, \"fourth\" text, \"fifth\" text);"
+sqlite3 $database_file_name "create table word_counts (\"word\" text, \"first\" text, \"second\" text, \"third\" text, \"fourth\" text, \"fifth\" text);"
 cat wordle*.txt | csv_to_sqlite "$database_file_name" "$table_name"
 sqlite3 $database_file_name "insert into word_counts select word, substr(word, 1, 1), substr(word, 2, 1), substr(word, 3, 1), substr(word, 4, 1), substr(word, 5, 1) from words;"
 
